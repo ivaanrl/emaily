0248f7b59a1b06a518c50f19f7949214ccadf14c
ivaanrl <ivanrl2010@gmail.com>
2019-12-22 17:15:09 -0300

    w

--- services/passport.js
+++ services/passport.js
@@ -44,7 +44,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
+      callbackURL:
+        "https://morning-chamber-45724.herokuapp.com/auth/google/callback",
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {

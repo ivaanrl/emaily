3c8bb9a79d938741224c556c3d040bbf4543f6f9
ivaanrl <ivanrl2010@gmail.com>
2019-12-22 15:23:38 -0300

    pleas work

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
+  mongoURI: process.env.mongoURI,
+  cookieKey: process.env.cookieKey,
+  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET
+};

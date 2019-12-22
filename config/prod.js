688dd465e4850d9034ca406a8b63415de714ac45
ivaanrl <ivanrl2010@gmail.com>
2019-12-22 17:01:36 -0300

    could you please work

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,5 @@ module.exports = {
   mongoURI: process.env.mongoURI,
   cookieKey: process.env.cookieKey,
   FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
-  googleRedirectURI: "https://morning-chamber-45724.herokuapp.com",
   FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET
 };

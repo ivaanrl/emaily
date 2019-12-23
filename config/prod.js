1b2fe33357e0b983114ad911b6ea595d04579be6
ivaanrl <ivanrl2010@gmail.com>
2019-12-23 13:40:25 -0300

    Fix Google API redirection... I think

--- config/prod.js
+++ config/prod.js
@@ -1,16 +1,8 @@
 module.exports = {
-  //googleClientID: process.env.GOOGLE_APP_ID,
-  //googleClientSecret: process.env.GOOGLE_APP_SECRET,
-  //mongoURI: process.env.mongo_URI,
-  //cookieKey: process.env.cookie_KEY,
-  //FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
-  //FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET
-  googleClientID:
-    "1022670959347-3esqcncfur5jh6kqkdsvk4v4jq38idpm.apps.googleusercontent.com",
-  googleClientSecret: "0jf9PkGTJZkCEKUpeGinpnHK",
-  mongoURI:
-    "mongodb+srv://new_user_31:AnEzaVLeXkIciS4U@devconector-btgri.mongodb.net/test?retryWrites=true&w=majority",
-  cookieKey: "skudrhjgfisukjefheswikdfhazlkdjhalskdh",
-  FACEBOOK_APP_ID: "2608059989429747",
-  FACEBOOK_APP_SECRET: "fedda47ee55cc946d2ac414caafd8911"
+  googleClientID: process.env.GOOGLE_APP_ID,
+  googleClientSecret: process.env.GOOGLE_APP_SECRET,
+  mongoURI: process.env.mongo_URI,
+  cookieKey: process.env.cookie_KEY,
+  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET
 };

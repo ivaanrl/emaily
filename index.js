1f38a7137e483e3526819fb1ad7b7aa408e17842
ivaanrl <ivanrl2010@gmail.com>
2019-12-23 17:40:20 -0300

    trying to fix

--- index.js
+++ index.js
@@ -32,9 +32,10 @@ if (process.env.NODE_ENV === "production") {
 
   //Express will serve up the index.html if it doesn't
   //recognize the route
-  const path = require("path");
+  const root = require("path").join(__dirname, "client", "build");
+  app.use(express.static(root));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile("index.html", { root });
   });
 }
 

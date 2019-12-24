410b51c93354e881d12925bb106dbafff6c08881
ivaanrl <ivanrl2010@gmail.com>
2019-12-23 21:00:41 -0300

    Credit system implemented. Started adding Mailer

--- config/prod.js
+++ config/prod.js
@@ -6,5 +6,6 @@ module.exports = {
   FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
   FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 };
